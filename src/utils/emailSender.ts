f702e6dbcb6e93b83bb6f5d4fb1fc50b905b0524
jinmo0320 <61862525+jinmo0320@users.noreply.github.com>
2025-12-13 23:29:59 +0900

    [Fix] AWS SES ENV & ERROR 처리 (#33)

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -9,8 +9,8 @@ dotenv.config();
 const sesClient = new SESClient({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    accessKeyId: process.env.SES_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.SES_SECRET_ACCESS_KEY || "",
   },
 });
 
